1dfb28f525995760effcda36efe63ac0adaf502b
Vitor <vitorhuculak14@gmail.com>
2025-10-10 17:41:00 -0300

    ensaio de testes

---
+++ testes.js
@@ -0,0 +1,32 @@
+//var agora = new Date()
+var diaSem = 'Sabado'
+
+//console.log(diaSem)
+
+switch(diaSem) {
+    
+        case 'Domingo':
+        console.log('Domingo')
+            break
+        case 'Segunda':
+        console.log('Segunda')
+            break
+        case 'Terça':
+        console.log('Terça')
+            break
+        case 'Quarta':
+        console.log('Quarta')
+            break
+        case 'Quinta':
+        console.log('Quinta')
+            break
+        case 'Sexta':
+        console.log('Sexta')
+            break
+        case 'Sabado':
+        console.log('Sabado')
+            break
+        default:
+            console.log('[ERRO] Dia Invalido')
+        break
+}
