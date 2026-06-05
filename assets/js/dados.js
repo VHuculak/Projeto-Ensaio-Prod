dfea9e9607b5f21abb8f0d6cbd3e1bbb403b6da4
Vitor Huçulak <vitorhuculak14@gmail.com>
2026-06-05 10:19:14 -0300

    subindo novas features para prod

---
+++ assets/js/dados.js
@@ -0,0 +1,290 @@
+const dadosCongregacoes = [
+    {
+        id: "culto-vila-santo-antonio",
+        nome: "Vila Santo Antônio",
+        endereco: "Rua Aristides Lobo, 776 - Maringá - PR - 87030-240",
+        anciao: "Luiz dos Santos Mendes\nLaerte de Oliveira Tolentino\nSalatiel Farias Dias",
+        cooperador: "",
+        diacono: "Geraldo Sales da Silva\nEnéias Bahia de Oliveira",
+        jovens: "Josué Vitorino Serra"
+    },
+    {
+        id: "culto-jardim-vitoria",
+        nome: "Jardim Vitória",
+        endereco: "Rua Professora Letícia de Paula Andrade Molinari, 883 - Maringá - PR - 87023-623",
+        anciao: "",
+        cooperador: "",
+        diacono: "Álvaro Zetoles\nPaulo Sergio Ribeiro Sampaio",
+        jovens: "Jean Carlos Santos da Silva"
+    },
+    {
+        id: "culto-jardim-veronica",
+        nome: "Jardim Verônica",
+        endereco: "Rua Pioneiro José Demori, 2211 - Jardim Verônica, Maringá - PR - 87060-150",
+        anciao: "",
+        cooperador: "Valentin Dias Canhamero\nMarcos Antonio Muniz de Oliveira",
+        diacono: "Francisco Roberto da Silva",
+        jovens: "Ricardo Belucci"
+    },
+    {
+        id: "culto-jardim-alvorada-ebenezer",
+        nome: "Jardim Alvorada III - Ebenezer",
+        endereco: "Rua Pioneiro Porphirio de Moraes, 750 - Maringá - PR - 87035-375",
+        anciao: "",
+        cooperador: "Marcos de Oliveira Silva",
+        diacono: "Sandro Ricardo Grazilio",
+        jovens: "Edmilson João da Silva"
+    },
+    {
+        id: "culto-aeroporto-3",
+        nome: "Parque Residencial Aeroporto - 3ª Parte",
+        endereco: "Rua José Peralta Pardial, 181 - Jardim Bertioga, Maringá - PR - 87055-180",
+        anciao: "Izaías Valério",
+        cooperador: "Elias Da Silva Felipe",
+        diacono: "Darcy Ribeiro de Melo",
+        jovens: "Joelton dos Santos Taraciuk"
+    },
+    {
+        id: "culto-parque-das-laranjeiras",
+        nome: "Parque das Laranjeiras",
+        endereco: "Rua Pioneiro Lívio Olívio, 169 - Maringá - PR - 87083-100",
+        anciao: "Marcio Roberto Valério",
+        cooperador: "Closneir Regilio de Souza",
+        diacono: "Samuel Alves da Silva\nRinaldo Gonçalves de Barros",
+        jovens: "Pedro Henrique Lanjoni Barros"
+    },
+    {
+        id: "culto-jardim-piata",
+        nome: "Jardim Piatã",
+        endereco: "Rua Pioneiro Gregório Moreira, 700 - Maringá - PR - 87043-575",
+        anciao: "",
+        cooperador: "Hélio dos Santos Reis",
+        diacono: "José Edson dos Santos",
+        jovens: "Fabiano Pereira da Silva"
+    },
+    {
+        id: "culto-jardim-olimpico",
+        nome: "Jardim Olímpico",
+        endereco: "Avenida Naihma Name, 474 - Maringá - PR - 87070-410",
+        anciao: "",
+        cooperador: "Jonathan Hoffmann Gomes",
+        diacono: "",
+        jovens: "Josué Cândido"
+    },
+    {
+        id: "culto-parque-taruma",
+        nome: "Parque Tarumã",
+        endereco: "Rua Pioneiro João Custódio Pereira, 1055 - Maringá - PR - 87053-590",
+        anciao: "",
+        cooperador: "Josué dos Santos Rocha",
+        diacono: "",
+        jovens: "George Adriano Cruz Barros"
+    },
+    {
+        id: "culto-jardim-indaia",
+        nome: "Jardim Indaiá",
+        endereco: "Avenida Pioneiro João Pereira, 902 - Maringá - PR - 87070-660",
+        anciao: "",
+        cooperador: "Sidney Medeiros",
+        diacono: "",
+        jovens: "Cidnei Real Peres"
+    },
+    {
+        id: "culto-centro-central",
+        nome: "Centro - Central",
+        endereco: "Rua Arthur Thomas, 903 - Maringá - PR - 87013-250",
+        anciao: "Geter Valério\nGustavo Pereira Corral Fernandes",
+        cooperador: "Mário Cesar Valério",
+        diacono: "Benedito José dos Santos\nJonas Lemos\nEliasor Valério\nDavid Santos Pinto\nHelio dos Santos\nClosnivaldo Regilio de Souza",
+        jovens: "Vinicius Segantine Busatto Pereira"
+    },
+    {
+        id: "culto-vila-operaria",
+        nome: "Vila Operária",
+        endereco: "Rua Santos Dumont, 141 - Maringá - PR - 87050-100",
+        anciao: "",
+        cooperador: "Antônio Silva Cardoso",
+        diacono: "Fernando Garcia Gonçalves",
+        jovens: "Jhonatas Macário Guandaline"
+    },
+    {
+        id: "culto-jardim-alvorada",
+        nome: "Jardim Alvorada",
+        endereco: "Rua Mato Grosso, 1182 - Maringá - PR - 87033-110",
+        anciao: "Nelson Pereira",
+        cooperador: "Valdir Gaspar\nErly Silva Fabiano",
+        diacono: "Elias do Nascimento Oliveira",
+        jovens: "Luiz Henrique Perazolo"
+    },
+    {
+        id: "culto-vila-santa-izabel",
+        nome: "Vila Santa Izabel",
+        endereco: "Rua João de Matos Alberto, 148 - Vila Santa Izabel, Maringá - PR - 87080-460",
+        anciao: "Melchior José Alves",
+        cooperador: "Lavio de Oliveira Tolentino",
+        diacono: "João de Oliveira Rodrigues\nValtoir da Silva Fernandes",
+        jovens: "Alair Jovino de Carvalho"
+    },
+    {
+        id: "culto-floriano",
+        nome: "Distrito de Floriano",
+        endereco: "R. Patu, 504 - Centro, Maringá - PR, 87105-046",
+        anciao: "",
+        cooperador: "Vitor Moisés Honorato",
+        diacono: "",
+        jovens: "Osvaldo Fernandes de Oliveira"
+    },
+    {
+        id: "culto-conjunto-requiao",
+        nome: "Conjunto Requião",
+        endereco: "Rua Major Abelardo José Cruz, 2435 - Maringá - PR - 87047-426",
+        anciao: "",
+        cooperador: "Luiz Fernandes Cubero",
+        diacono: "",
+        jovens: "Sidney Dias"
+    },
+    {
+        id: "culto-jardim-universo",
+        nome: "Jardim Universo",
+        endereco: "Rua Universo, 1095 - Maringá - PR - 87060-420",
+        anciao: "",
+        cooperador: "Eber Luiz Moro",
+        diacono: "Abner Meneghetti",
+        jovens: "Osvanil Ruiz Molina"
+    },
+    {
+        id: "culto-parque-avenida",
+        nome: "Parque Avenida",
+        endereco: "Rua Allan Kardec, 1827 - Maringá - PR - 87025-410",
+        anciao: "",
+        cooperador: "Samuel do Carmo da Silva",
+        diacono: "José de Oliveira Santos",
+        jovens: "Clayton da Costa"
+    },
+    {
+        id: "culto-jardim-oriental-diamante",
+        nome: "Jardim Oriental - Diamante",
+        endereco: "Avenida São Judas Tadeu, 2911 - Maringá - PR - 87024-213",
+        anciao: "",
+        cooperador: "Fredmann Luiz Favaro",
+        diacono: "Luis Alberto de Lima",
+        jovens: "Moisés Rodrigues Zetoles"
+    },
+    {
+        id: "culto-jardim-espanha",
+        nome: "Jardim Espanha",
+        endereco: "Avenida José Marchi, 164 - Maringá - PR - 87060-690",
+        anciao: "",
+        cooperador: "José Carlos Donizeti de Souza",
+        diacono: "",
+        jovens: ""
+    },
+    {
+        id: "culto-iguatemi",
+        nome: "Distrito de Iguatemi",
+        endereco: "Rua Pioneiro Anselmo Cantagalli, 140 - Maringá - PR - 87103-080",
+        anciao: "José Antônio dos Reis",
+        cooperador: "Clemente Alves de Araújo",
+        diacono: "Anderson José Alves",
+        jovens: "José Francisco Ferreira"
+    },
+    {
+        id: "culto-vila-morangueira",
+        nome: "Vila Morangueira",
+        endereco: "Rua Nivaldo Aparecido Lopes, 805 - Maringá - PR - 87040-095",
+        anciao: "Clóvis Maciel de Oliveira",
+        cooperador: "Alexandro Rissi Petta",
+        diacono: "Oswaldo Campano",
+        jovens: "Daniel Lial Ricardo"
+    },
+    {
+        id: "culto-parque-tuiuti",
+        nome: "Parque Residencial Tuiuti",
+        endereco: "Rua Rio Ligeiro, 1003 - Maringá - PR - 87043-200",
+        anciao: "",
+        cooperador: "Natanael Silva dos Santos",
+        diacono: "Rodrigo Camargo Amoroso",
+        jovens: "Jean Ricardo Costa"
+    },
+    {
+        id: "culto-jardim-liberdade",
+        nome: "Jardim Liberdade",
+        endereco: "Rua Gilson Ricardo Gomes de Castro, 335 - Maringá - PR - 87047-270",
+        anciao: "Aldemar Meneghetti",
+        cooperador: "João Mendes Fonseca",
+        diacono: "Eronides Soares",
+        jovens: "Richarlles Celso Rodrigues Pereira"
+    },
+    {
+        id: "culto-parque-itaipu",
+        nome: "Parque Itaipu",
+        endereco: "Rua Pioneiro José dos Santos, 313 - Maringá - PR - 87065-440",
+        anciao: "",
+        cooperador: "Dilson Miliorini\nJoab dos Santos",
+        diacono: "Amadeu Campos dos Santos\nJoel Martins",
+        jovens: "Edson Barboza"
+    },
+    {
+        id: "culto-parque-hortencia-1",
+        nome: "Parque Hortência - 1ª Parte",
+        endereco: "Rua Pintagol, 701 - Maringá - PR - 87075-300",
+        anciao: "Valter Luis de Araújo",
+        cooperador: "Leandro Fertonani Lemos",
+        diacono: "Felipe Rocengholli Valentim",
+        jovens: "Osvaldo Rodrigues de Oliveira Júnior"
+    },
+    {
+        id: "culto-jardim-catedral",
+        nome: "Jardim Catedral",
+        endereco: "Rua Pioneiro Olivar Freitas de Paiva, 104 - Maringá - PR - 87053-375",
+        anciao: "",
+        cooperador: "José Nogueira da Silva",
+        diacono: "",
+        jovens: "Israel Alonso de Albuquerque"
+    },
+    {
+        id: "culto-conjunto-guaiapo",
+        nome: "Conjunto Residencial Guaiapó",
+        endereco: "Avenida Franklin Delano Roosevelt, 5605 - Maringá - PR - 87047-420",
+        anciao: "",
+        cooperador: "Devanir Batista da Silva",
+        diacono: "",
+        jovens: "Israel Dias"
+    },
+    {
+        id: "culto-jardim-ouro-cola",
+        nome: "Jardim Ouro Cola",
+        endereco: "Avenida Virgílio Manília, 22104 - Maringá - PR - 87070-170",
+        anciao: "João Marinho",
+        cooperador: "José Silva dos Santos",
+        diacono: "",
+        jovens: "Paulo Cezar Panaro"
+    },
+    {
+        id: "culto-jardim-monte-rei",
+        nome: "Jardim Monte Rei",
+        endereco: "Rua Pioneiro Olinto Mariani, 1888 - Maringá - PR - 87083-675",
+        anciao: "",
+        cooperador: "Flávio Candido Mesquita\nClaudio Medina",
+        diacono: "Luiz Armando de Lima",
+        jovens: "Jayme Manoel Soares Junior"
+    },
+    {
+        id: "culto-maringa-velho",
+        nome: "Zona 6 - Maringá Velho",
+        endereco: "Rua Raimundo Correia, 708 - Maringá - PR - 87015-400",
+        anciao: "",
+        cooperador: "Douglas Godoy de Azevedo",
+        diacono: "",
+        jovens: "Vilmar Lucas Mota Sampaio"
+    },
+    {
+        id: "culto-loteamento-sumare",
+        nome: "Loteamento Sumaré",
+        endereco: "Rua José Leonidas Nerone, 631 - Maringá - PR - 87035-450",
+        anciao: "",
+        cooperador: "Eduardo Cesar de Gouvea",
+        diacono: "",
+        jovens: "Ailton de Souza Silva"
+    }
+];
---
+++ assets/js/script.js
@@ -0,0 +1,170 @@
+// --- Elementos da Página ---
+const paginaCultos = document.getElementById('pagina-cultos');
+const paginaEnsaios = document.getElementById('pagina-ensaios');
+const paginaEventos = document.getElementById('pagina-serviços');
+const modalOverlay = document.getElementById('modal-overlay');
+const modalFechar = document.getElementById('modal-fechar');
+
+// --- Esconder abas para navegação ---
+
+function mostrarCultos() {
+    paginaEventos.classList.add('hidden');
+    paginaEnsaios.classList.add('hidden');
+    paginaCultos.classList.remove('hidden');
+}
+
+function mostrarEnsaios() {
+    paginaCultos.classList.add('hidden');
+    paginaEventos.classList.add('hidden');
+    paginaEnsaios.classList.remove('hidden');
+}
+
+function mostrarServiços() {
+    paginaEnsaios.classList.add('hidden');
+    paginaCultos.classList.add('hidden');
+    paginaEventos.classList.remove('hidden');
+}
+
+ /* Remove acentos para facilitar a busca */
+function removerAcentos(texto) {
+    if (!texto) return "";
+    // NFD separa o acento da letra, e o Regex substitui o acento por vazio
+    return texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+
+function configurarBusca(idInput, idLista) {
+    const input = document.getElementById(idInput);
+    
+    // Segurança: se o input não existir na página, para a execução
+    if (!input) return; 
+
+    const listaItens = document.getElementById(idLista).getElementsByTagName('li');
+
+    input.addEventListener('input', function() {
+        const termoBuscado = input.value.toLowerCase();
+        const termoSemAcento = removerAcentos(termoBuscado);
+        
+        // Separa o que foi digitado por espaço para buscar palavras fora de ordem
+        const palavrasBuscadas = termoSemAcento.split(' ');
+
+        for (let i = 0; i < listaItens.length; i++) {
+            const item = listaItens[i];
+            const textoItem = item.textContent.toLowerCase();
+            const textoItemSemAcento = removerAcentos(textoItem);
+
+            let ehUmResultadoValido = true;
+
+            // Lógica "E": O item precisa conter TODAS as palavras digitadas
+            for (const palavra of palavrasBuscadas) {
+                if (!textoItemSemAcento.includes(palavra)) {
+                    ehUmResultadoValido = false;
+                    break;
+                }
+            }
+
+            if (ehUmResultadoValido) {
+                item.classList.remove('hidden');
+            } else {
+                item.classList.add('hidden');
+            }
+        }
+    });
+}
+
+function obterDiaSemanaAtual() {
+    const dias = ['domingo','segunda','terca','quarta','quinta','sexta','sabado'];
+    return dias[new Date().getDay()];
+}
+
+function formatarDataHoje() {
+    const hoje = new Date();
+    const dia = String(hoje.getDate()).padStart(2, '0');
+    const mes = String(hoje.getMonth() + 1).padStart(2, '0');
+    const ano = String(hoje.getFullYear()).slice(-2);
+    return `${dia}/${mes}/${ano}`;
+}
+
+function obterListaVisivel() {
+    if (!paginaCultos.classList.contains('hidden')) return document.getElementById('lista-cultos');
+    if (!paginaEnsaios.classList.contains('hidden')) return document.getElementById('lista-ensaios');
+    if (!paginaEventos.classList.contains('hidden')) return document.getElementById('lista-serviços');
+    return null;
+}
+
+function filtrarHoje() {
+    const diaSemana = obterDiaSemanaAtual();
+    const dataHoje = formatarDataHoje();
+    const listaVisivel = obterListaVisivel();
+
+    if (!listaVisivel) return;
+
+    const itens = listaVisivel.getElementsByTagName('li');
+    for (let i = 0; i < itens.length; i++) {
+        const item = itens[i];
+        const textoItemSemAcento = removerAcentos(item.textContent.toLowerCase());
+
+        const contemDia = textoItemSemAcento.includes(diaSemana);
+        const contemData = textoItemSemAcento.includes(dataHoje);
+
+        if (contemDia || contemData) {
+            item.classList.remove('hidden');
+        } else {
+            item.classList.add('hidden');
+        }
+    }
+}
+
+function limparFiltro() {
+    const listaIds = ['lista-cultos', 'lista-ensaios', 'lista-serviços'];
+    listaIds.forEach(function(id) {
+        const lista = document.getElementById(id);
+        if (!lista) return;
+        const itens = lista.getElementsByTagName('li');
+        for (let i = 0; i < itens.length; i++) {
+            itens[i].classList.remove('hidden');
+        }
+    });
+
+    ['busca-cultos', 'busca-ensaio', 'busca-serviços'].forEach(function(id) {
+        const input = document.getElementById(id);
+        if (input) {
+            input.value = '';
+        }
+    });
+}
+
+function fecharModal() {
+    modalOverlay.classList.add('hidden');
+    document.body.style.overflow = '';
+}
+
+modalFechar.addEventListener('click', fecharModal);
+
+function abrirModal(id){
+    const congregacao = dadosCongregacoes.find(item => item.id === id);
+
+    document.getElementById('modal-nome').textContent = congregacao.nome;
+    document.getElementById('modal-endereco').textContent = congregacao.endereco;
+    document.getElementById('modal-anciao').textContent = congregacao.anciao;
+    document.getElementById('modal-cooperador').textContent = congregacao.cooperador;
+    document.getElementById('modal-diacono').textContent = congregacao.diacono;
+    document.getElementById('modal-jovens').textContent = congregacao.jovens;
+
+    modalOverlay.classList.remove('hidden');
+
+    document.getElementById('modal-maps').href = `https://maps.google.com/?q=${congregacao.endereco.replace(/ /g, '+')}`;
+    document.body.style.overflow = 'hidden';
+
+}
+
+const itensCultos = document.getElementById('lista-cultos').getElementsByTagName('li');
+for (let i = 0; i < itensCultos.length; i++) {
+    itensCultos[i].addEventListener('click', function() {
+        abrirModal(this.id);
+})
+};
+
+// --- Inicialização das Buscas ---
+configurarBusca('busca-cultos', 'lista-cultos');
+configurarBusca('busca-ensaio', 'lista-ensaios');
+configurarBusca('busca-serviços', 'lista-serviços');
